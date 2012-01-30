923aaeb293bc9915bb9b947c7cf26c2c0dce38dc
Albert Callarisa Roca <shark234@gmail.com>
2012-01-30 19:25:02 +0800

    Added creation of notifications functionality

--- lib/socifier.rb
+++ lib/socifier.rb
@@ -1,13 +1,36 @@
 require "socifier/version"
+require "rest-client"
 
 module Socifier
+  class NoApiKeySpecified < Exception; end
+  class InvalidParams < Exception; end
+
   CONFIGURATION_KEYS = [:api_key]
+  SOCIFIER_PATH = "http://www.socifier.com"
+
   class << self
     attr_accessor :configuration
 
     def configure(&block)
       self.configuration ||= Struct.new("Configuration", *CONFIGURATION_KEYS).new
       block.call self.configuration
+    end
+
+    def new_socification(params)
+      raise NoApiKeySpecified if self.configuration.api_key.nil?
+      raise InvalidParams if params[:id].empty? || params[:title].empty?
+
+      query = {
+        auth_token: self.configuration.api_key,
+        title: params[:title],
+        id: params[:id],
+        is_recurrent: (params[:is_recurrent] ? "1" : "0")
+      }
+      url = "#{Socifier::SOCIFIER_PATH}/api/v1/socifications"
+      response_code = RestClient.post(url, query) do |response, request, result|
+        response.code
+      end
+      response_code == 201
     end
   end
 end
--- spec/socifier_spec.rb
+++ spec/socifier_spec.rb
@@ -1,12 +1,63 @@
 require 'spec_helper'
 
 describe Socifier do
+  let(:api_key) { "my_api_key" }
+  let(:response_code) { 200 }
+  before do
+    Socifier.configure{|c| c.api_key = api_key }
+    RestClient.stub(:post).and_return(response_code)
+    RestClient.stub(:put).and_return(response_code)
+  end
+
   describe ".configure" do
     subject { Socifier.configuration }
 
     describe "api_key" do
-      before { Socifier.configure{|c| c.api_key = "my_api_key" } }
       its(:api_key) { should == "my_api_key" }
+    end
+  end
+
+  describe ".new_socification" do
+    it "calls the API endpoint" do
+      RestClient.should_receive(:post).with("#{Socifier::SOCIFIER_PATH}/api/v1/socifications", {
+        auth_token: "my_api_key",
+        title: "Test socification",
+        id: "test",
+        is_recurrent: "1"})
+      Socifier.new_socification id: "test", title: "Test socification", is_recurrent: true
+    end
+
+    context "without API key" do
+      let(:api_key) { nil }
+      it "raises an exception" do
+        expect {
+          Socifier.new_socification id: "test", title: "Test socification", is_recurrent: true
+        }.to raise_exception(Socifier::NoApiKeySpecified)
+      end
+    end
+
+    context "invalid params" do
+      it "requires title" do
+        expect {
+          Socifier.new_socification id: "test", title: "", is_recurrent: true
+        }.to raise_exception(Socifier::InvalidParams)
+      end
+      it "requires id" do
+        expect {
+          Socifier.new_socification id: "", title: "Test socification", is_recurrent: true
+        }.to raise_exception(Socifier::InvalidParams)
+      end
+    end
+
+    describe "response" do
+      let(:response_code) { 201 }
+      subject { Socifier.new_socification(id: "test", title: "Test socification", is_recurrent: true) }
+      it { should be_true }
+
+      context "failed request" do
+        let(:response_code) { 403 }
+        it { should be_false }
+      end
     end
   end
 end
